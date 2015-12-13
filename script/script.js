010d8f85e28b6fcaf0b216f1b5141a25cf896f69
girishkuniyal <girishchandrakuniyal@gmail.com>
2015-12-13 19:29:09 +0530

    front page box ,color remove

--- script/script.js
+++ script/script.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
-  $('.headBox').addClass('redeffect');
-  $('g').addClass("w");
-  $('.headBox').fadeTo(250,0.75);
-   $('.headBox').mouseenter(function() {
-       $(this).fadeTo(250,1);;
-       $(this).removeClass('redeffect');
-   });
-   $('.headBox').mouseleave(function(){
-  $('.headBox').fadeTo(250,0.75);
-      $(this).addClass('redeffect');
-   });
-   
-   
-});
+$(document).ready(function(){
   
+
+  $(".headBox").mouseenter(function(){;
+     $(this).fadeTo(250,0.75);
+  });
+
+
+  $(".headBox").mouseleave(function(){
+    $(this).fadeTo(250,1);
+  });
+
+
+
+
+});
