37aa48aee526baeeda0096337cf2b24d7858dbdd
girishkuniyal <girishchandrakuniyal@gmail.com>
2016-01-19 15:42:59 +0530

    website icon layout and effect

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   
 
-  $(".headBox").mouseenter(function(){;
+  $(".headBox").mouseenter(function(){
      $(this).fadeTo(250,0.75);
   });
 
@@ -9,7 +9,82 @@ $(document).ready(function(){
   $(".headBox").mouseleave(function(){
     $(this).fadeTo(250,1);
   });
+/*************************************************************************************************************
+					menu effects
+*************************************************************************************************************/
+		
+		/*******************twitter*********************/
 
+	  $(".twitterStyle").mouseenter(function(){
+     	$(this).fadeTo(250,0.75);
+  	});
+
+
+	$(".twitterStyle").mouseleave(function(){
+    	$(this).fadeTo(250,1);
+  	});
+
+
+	
+		/*******************facebook*********************/
+
+	  $(".facebookStyle").mouseenter(function(){
+     	$(this).fadeTo(250,0.75);
+  	});
+
+	$(".facebookStyle").mouseleave(function(){
+	$(this).fadeTo(250,1);
+	})
+		/*******************Linkedin*********************/
+	
+	  $(".linkedinStyle").mouseenter(function(){
+     	$(this).fadeTo(250,0.75);
+  	});
+
+	$(".linkedinStyle").mouseleave(function(){
+    	$(this).fadeTo(250,1);
+  	});
+
+		/*******************blogs*********************/
+
+	  $(".blogStyle").mouseenter(function(){
+     	$(this).fadeTo(250,0.75);
+  	});
+		
+	$(".blogStyle").mouseleave(function(){
+    	$(this).fadeTo(250,1);
+  	});
+
+		/*******************github*********************/
+
+	  $(".githubStyle").mouseenter(function(){
+     	$(this).fadeTo(250,0.75);
+  	});
+		
+	$(".githubStyle").mouseleave(function(){
+    	$(this).fadeTo(250,1);
+  	});
+
+		/*******************mail*********************/
+
+	  $(".mailStyle").mouseenter(function(){
+     	$(this).fadeTo(250,0.75);
+  	});
+
+	$(".mailStyle").mouseleave(function(){
+    	$(this).fadeTo(250,1);
+  	});
+
+		/*******************resumer*********************/
+
+	  $(".resumeStyle").mouseenter(function(){
+     	$(this).fadeTo(250,0.75);
+  	});
+
+	$(".resumeStyle").mouseleave(function(){
+    	$(this).fadeTo(250,1);
+  	});
+	  	
 
 
 
